7a55507185087847d2841697c3ff1b973b66d551
Allison Zec <ajz3ee@virginia.edu>
2022-11-03 18:44:40 -0400

    Updated kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -1,6 +1,7 @@
 from math import sin
 from math import radians
 from math import sqrt
+from math import tan
 
 
 Mp = 938.272
@@ -14,7 +15,7 @@ def nu(E, Ep):
 	return E - Ep
 
 
-def W(E, Ep, theta):
+def Wsq(E, Ep, theta):
 	return Mp**2 + 2*Mp*nu(E, Ep) - Qsq(E, Ep, theta)
 
 
@@ -24,3 +25,12 @@ def xbj(E, Ep, theta):
 
 def gamma(E, Ep, theta):
 	return sqrt(Qsq(E, Ep, theta))/nu(E, Ep)
+
+
+def epsilon(E, Ep, theta):
+	fac = 1 + nu(E, Ep)**2/Qsq(E, Ep, theta)
+	return 1/(1 + fac*tan(radians(theta/2))**2)
+
+
+def kappa(E, Ep, theta):
+	return 1 - Qsq(E, Ep, theta)/(nu(E, Ep)**2)
